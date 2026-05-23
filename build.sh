cc8f34015f8a2d9ec0fbd46aa2acef14135acd32
maozi-114 <3078696784@qq.com>
2026-05-23 22:00:50 +0800

    Update build.sh
    add build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 export ARCH=arm64
 export SUBARCH=arm64
-export PATH="/home/maozi/android-gcc-4.9/bin:$PATH"
+export PATH="android-gcc-4.9/bin:$PATH"
 export CC="gcc"
 export CROSS_COMPILE="aarch64-linux-android-"
 export TRIPLE="aarch64-linux-android-"
